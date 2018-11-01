b35c721c044ae48db3e3af0e9fa20ec9537b2d1f
elishaday18 <elishamday18@gmail.com>
2018-10-31 22:24:32 -0400

    added truffle and created approvalContract
    added truffle back becuase it didnt fix configuration error in node. Created new solidity contract ApprovalContract.sol  that creates deposit function for the sender and receiver

---
+++ 03_02_start/contract/contracts/ApprovalContract.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.18;
+
+contract ApprovalContract{
+	address public sender;
+	address public reciever;
+	address public constant approver;
+	
+	function deposit(address _receiver) external payable {
+		require(msg.value>0);
+		semder = msg.sender;
+		receiver = _reciever;
+
+	}
+	function viewApproval() external pure returns(address){
+	return(approver);
+
+	}
+	function approve() external {
+	require(msg.sender == approval);
+	receiver.transfer(address(this).balance);
+	}	
+}
